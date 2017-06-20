f0bc1df1db37f613cfac91bdbd38e9a079113b5f
Jack Wilsdon <jack.wilsdon@gmail.com>
2017-06-21 00:09:51 +0100

    Add function to get a repository for a URL

--- svnwatch/repository.go
+++ svnwatch/repository.go
@@ -12,6 +12,21 @@ type Repositories struct {
 	Repositories []Repository `xml:"repository"`
 }
 
+func (r *Repositories) ForURL(url string) *Repository {
+	for i, _ := range r.Repositories {
+		if url == r.Repositories[i].URL {
+			return &r.Repositories[i]
+		}
+	}
+
+	r.Repositories = append(r.Repositories, Repository{
+		Revision: 0,
+		URL:      url,
+	})
+
+	return &r.Repositories[len(r.Repositories)-1]
+}
+
 type Repository struct {
 	XMLName  xml.Name `xml:"repository"`
 	Revision int      `xml:"revision,attr"`
