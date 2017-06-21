5a552cc7b429c36a97e9a032445f95cc9192a3c3
Jack Wilsdon <jack.wilsdon@gmail.com>
2017-06-21 13:53:47 +0100

    Retrieve and store changed paths from the SVN log

--- svn/log.go
+++ svn/log.go
@@ -14,13 +14,29 @@ type Log struct {
 type LogEntry struct {
 	*Commit
 	XMLName xml.Name `xml:"logentry"`
+	Paths   Paths    `xml:"paths"`
 	Message string   `xml:"msg"`
+}
+
+type Paths struct {
+	XMLName xml.Name `xml:"paths"`
+	Paths   []Path   `xml:"path"`
+}
+
+type Path struct {
+	XMLName               xml.Name `xml:"path"`
+	TextModifications     bool     `xml:"text-mods,attr"`
+	Kind                  string   `xml:"kind,attr"`
+	CopyFromPath          *string  `xml:"copyfrom-path,attr,omitempty"`
+	CopyFromRevision      *int     `xml:"copyfrom-rev,attr,omitempty"`
+	Action                string   `xml:"action,attr"`
+	PropertyModifications bool     `xml:"prop-mods,attr"`
 }
 
 func GetLog(address string) (*Log, error) {
 	log := Log{}
 
-	if err := Execute(&log, "log", "--xml", address); err != nil {
+	if err := Execute(&log, "log", "--xml", "--verbose", address); err != nil {
 		return nil, errors.Wrapf(err, "failed to get log for %s", address)
 	}
 
