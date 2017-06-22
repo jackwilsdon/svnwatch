49d34b6bf391b6879cf17ad2a6c7ef057a3f60b8
Jack Wilsdon <jack.wilsdon@gmail.com>
2017-06-22 13:37:03 +0100

    Allow repository information to be passed as arguments

--- svnwatch/command.go
+++ svnwatch/command.go
@@ -5,14 +5,16 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 
 	shellwords "github.com/mattn/go-shellwords"
 	"github.com/pkg/errors"
 )
 
 type Command struct {
-	XMLName xml.Name `xml:"command"`
-	Command string   `xml:",chardata"`
+	XMLName      xml.Name `xml:"command"`
+	ArgumentType *string  `xml:"pass-type,attr,omitempty"`
+	Command      string   `xml:",chardata"`
 }
 
 func (c Command) Execute(repo Repository) error {
@@ -25,11 +27,17 @@ func (c Command) Execute(repo Repository) error {
 	cmd := exec.Command(pieces[0], pieces[1:]...)
 	cmd.Env = os.Environ()
 
-	cmd.Env = append(
-		os.Environ(),
-		fmt.Sprintf("SVN_URL=%s", repo.URL),
-		fmt.Sprintf("SVN_REVISION=%d", repo.Revision),
-	)
+	if c.ArgumentType == nil || *c.ArgumentType == "normal" {
+		cmd.Args = append(cmd.Args, repo.URL, strconv.Itoa(repo.Revision))
+	} else if *c.ArgumentType == "env" {
+		cmd.Env = append(
+			os.Environ(),
+			fmt.Sprintf("SVN_URL=%s", repo.URL),
+			fmt.Sprintf("SVN_REVISION=%d", repo.Revision),
+		)
+	} else {
+		return fmt.Errorf("invalid argument type %s for %s", *c.ArgumentType, c.Command)
+	}
 
 	if err := cmd.Run(); err != nil {
 		return errors.Wrapf(err, "failed to execute %s", c.Command)
