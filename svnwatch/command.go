2897777a0fc6b8eea91532c151c3ae9e8aa4d8d0
Jack Wilsdon <jack.wilsdon@gmail.com>
2017-06-29 01:05:35 +0100

    Validate argument type for commands during unmarshal

--- svnwatch/command.go
+++ svnwatch/command.go
@@ -14,7 +14,7 @@ import (
 
 type Command struct {
 	XMLName      xml.Name `xml:"command"`
-	ArgumentType *string  `xml:"pass-type,attr,omitempty"`
+	ArgumentType string   `xml:"pass-type,attr,omitempty"`
 	Command      string   `xml:",chardata"`
 }
 
@@ -28,21 +28,44 @@ func (c Command) Execute(repo Repository, revision svn.Revision) error {
 	cmd := exec.Command(pieces[0], pieces[1:]...)
 	cmd.Env = os.Environ()
 
-	if c.ArgumentType == nil || *c.ArgumentType == "normal" {
+	if c.ArgumentType == "normal" {
 		cmd.Args = append(cmd.Args, repo.URL, strconv.Itoa(repo.Revision))
-	} else if *c.ArgumentType == "env" {
+	} else if c.ArgumentType == "env" {
 		cmd.Env = append(
 			os.Environ(),
 			fmt.Sprintf("SVN_URL=%s", repo.URL),
 			fmt.Sprintf("SVN_REVISION=%d", repo.Revision),
 		)
 	} else {
-		return fmt.Errorf("invalid argument type \"%s\" for \"%s\"", *c.ArgumentType, c.Command)
+		return fmt.Errorf("invalid argument type \"%s\" for \"%s\"", c.ArgumentType, c.Command)
 	}
 
 	if err := cmd.Run(); err != nil {
 		return errors.Wrapf(err, "failed to execute \"%s\"", c.Command)
 	}
+
+	return nil
+}
+
+func (c *Command) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	cmd := struct {
+		ArgumentType *string `xml:"pass-type,attr,omitempty"`
+		Command      string  `xml:"chardata"`
+	}{}
+
+	if err := d.DecodeElement(&cmd, &start); err != nil {
+		return err
+	}
+
+	if cmd.ArgumentType == nil {
+		c.ArgumentType = "normal"
+	} else if *cmd.ArgumentType == "normal" || *cmd.ArgumentType == "env" {
+		c.ArgumentType = *cmd.ArgumentType
+	} else {
+		return fmt.Errorf("invalid argument type \"%s\" for \"%s\"", *cmd.ArgumentType, cmd.Command)
+	}
+
+	c.Command = cmd.Command
 
 	return nil
 }
